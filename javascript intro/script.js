e8abf5ee1a239bd90d58a76d0732d0dc414f0833
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2024-10-13 22:59:02 +0530

    intro to javascript

---
+++ javascript intro/script.js
@@ -0,0 +1,108 @@
+// //what is javascript. javascript is a programming language. 
+// //it is used to calculate , mathematical operations,controlling css and html,add events and functions
+// // to the webpages.
+
+
+
+// // varaibles , data types
+
+// let a = 10
+// let b = 2.01 // numbers
+// let c = -456
+// let name = 'Asim' // string '' or "" or ``
+// var car = "BMW"
+// let phone = `iPhone`
+
+// let isTrue = true // boolean
+// var day = false;
+// const pi = 3.14
+// const g = 9.8
+// {
+//     let x = 10
+//     var y = 20
+// }
+
+// // non premitive datatypes
+// let arr = [10, 20, 30, 40, 50]
+
+// let obj = {
+//     name: 'Asim',
+//     age: 25
+// }
+
+// console.log(typeof(name))
+// console.log(typeof(pi))
+// console.log(typeof(arr))
+// console.log(typeof(obj))
+// // operations
+//   // arthematic operations
+//    // + - / * % ++ -- **
+//    let x = 10
+//    console.log(x++)
+//    console.log(x)
+//    console.log('asim'+'sheikh')
+//    console.log('asim'-'sheikh')
+//    console.log('asim'*2)
+//    console.log(2**3)
+
+
+//    // conditons statements
+
+let coin = 'copper'
+
+if(coin=='heads'){
+    console.log('heads');
+}
+else if(coin=='tails'){
+    console.log('tails');
+}
+else{
+    console.log('invalid coin value');
+}
+
+let dice = 1000;
+
+switch(dice){
+    case 1:
+        console.log('move one step');
+        break
+    case 2:
+        console.log('move two step');
+        break
+    case 3: 
+        console.log('move three steps');
+        break
+    default:
+        console.log('invalid dice value');
+}
+
+let hungry = false;
+ 
+hungry? console.log('eat someting'): console.log('do something')
+
+// functions
+add(10,20)
+function add(a,b){
+    return a+b;
+    console.log('hu hahaah');
+}
+
+// anonymous function
+
+jump();
+let jump = function (){
+    console.log('jump jump jump');
+}
+
+
+
+let result = add(10,20)
+ console.log(result)
+// dom manipulation
+// events
+// objects
+// arrays
+// loops
+
+// async javascript
+// how javascript works
